b179bf898e407680eb268b1fad1223de813cdc75
s1300037 <s1300037@u-aizu.ac.jp>
2023-06-11 16:07:22 +0900

    Won/lost message added

--- dice.c
+++ dice.c
@@ -1,10 +1,19 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    int a,b;
+int main()
+{
+    int a, b;
     printf("Rolling dice...\n");
-    a = rand()%6 +1;
-    b = rand()%6 +1;
-    printf("Die 1: %d\nDie 2: %d\nTotal value: %d\n",a,b,a+b);
+    a = rand() % 6 + 1;
+    b = rand() % 6 + 1;
+    printf("Die 1: %d\nDie 2: %d\nTotal value: %d\n", a, b, a + b);
+    if (a + b > 7)
+    {
+        printf("You won\n");
+    }
+    else
+    {
+        printf("You lost");
+    }
 }
